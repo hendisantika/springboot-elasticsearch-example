9c768c8edfc5bd008d610a4eb064d118e8b868e1
Hendi Santika <hendisantika@gmail.com>
2019-12-26 02:52:02 +0700

    #3 - Add Book Domain

---
+++ src/main/java/com/hendisantika/springbootelasticsearchexample/domain/Book.java
@@ -0,0 +1,29 @@
+package com.hendisantika.springbootelasticsearchexample.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.elasticsearch.annotations.Document;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-elasticsearch-example
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 26/12/19
+ * Time: 02.50
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Document(indexName = "hendi", type = "books")
+public class Book {
+    @Id
+    private String id;
+    private String title;
+    private String author;
+    private String releaseDate;
+
+}

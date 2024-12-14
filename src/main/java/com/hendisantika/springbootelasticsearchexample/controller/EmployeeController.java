13eeda36e0729a13ef96640bb70ef7ecb62e5f0d
hendisantika <hendisantika@yahoo.co.id>
2024-12-14 11:36:37 +0700

    feat: Add update in EmployeeController🫸🌀✏️📗🐧🐳⬆

--- src/main/java/com/hendisantika/springbootelasticsearchexample/controller/EmployeeController.java
+++ src/main/java/com/hendisantika/springbootelasticsearchexample/controller/EmployeeController.java
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -42,4 +43,11 @@ public class EmployeeController {
     public Iterable<Employee> findAll() {
         return repository.findAll();
     }
+
+    @PutMapping("/{id}")
+    public Employee update(@PathVariable String id, @RequestBody Employee employee) {
+        employee.setId(id);
+        return repository.save(employee);
+    }
+
 }

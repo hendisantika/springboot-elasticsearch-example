bca51e5fe9d149d03a6e97d7b7c8724cf74d4018
hendisantika <hendisantika@yahoo.co.id>
2024-12-14 11:27:11 +0700

    feat: Add save in ProductsController🫸🌀✏️📗🐧🐳⬆

--- src/main/java/com/hendisantika/springbootelasticsearchexample/controller/ProductsController.java
+++ src/main/java/com/hendisantika/springbootelasticsearchexample/controller/ProductsController.java
@@ -5,6 +5,8 @@ import com.hendisantika.springbootelasticsearchexample.domain.Product;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -28,5 +30,10 @@ public class ProductsController {
     @GetMapping("")
     public Iterable<Product> findAll() {
         return productRepository.findAll();
+    }
+
+    @PostMapping("")
+    public Product save(@RequestBody Product product) {
+        return productRepository.save(product);
     }
 }

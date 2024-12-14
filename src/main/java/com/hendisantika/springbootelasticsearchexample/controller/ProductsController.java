2215a54cde7aa5116c99464a5dd2ca63bf182526
hendisantika <hendisantika@yahoo.co.id>
2024-12-14 11:27:21 +0700

    feat: Add update in ProductsController🫸🌀✏️📗🐧🐳⬆

--- src/main/java/com/hendisantika/springbootelasticsearchexample/controller/ProductsController.java
+++ src/main/java/com/hendisantika/springbootelasticsearchexample/controller/ProductsController.java
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -35,5 +36,13 @@ public class ProductsController {
     @PostMapping("")
     public Product save(@RequestBody Product product) {
         return productRepository.save(product);
+    }
+
+    @PutMapping("update")
+    public Product update(@RequestBody Product product) throws Exception {
+        if (product.getId() != 0) {
+            return productRepository.save(product);
+        }
+        throw new Exception("Id is required");
     }
 }
